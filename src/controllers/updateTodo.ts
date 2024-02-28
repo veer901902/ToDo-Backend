af7987a4e56be865a7671905aa575b47f1e57043
veer901902 <veer901902@gmail.com>
2024-02-28 19:45:44 +0530

    Added the updateTodo route

--- src/controllers/updateTodo.ts
+++ src/controllers/updateTodo.ts
@@ -9,6 +9,28 @@ export default async function updateTodo(
   req: Request,
   res: Response,
   next: NextFunction
-){
-   
+) {
+  try {
+    const userId = req.user.id;
+    const { id, title, description, done } = req.body;
+
+    const dataToUpdate: {
+      title?: string;
+      description?: string;
+      done?: boolean;
+    } = {};
+    
+    if (title) dataToUpdate.title = title;
+    if (description) dataToUpdate.description = description;
+    if (done)
+      dataToUpdate.done = typeof done === "string" ? done === "true" : done;
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id: parseInt(id) },
+      data: dataToUpdate,
+    });
+    res.status(201).json(updatedTodo);
+  } catch (error) {
+    next(error);
+  }
 }
--- src/routes/toDoRoutes.ts
+++ src/routes/toDoRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import createTodo from "../controllers/createTodo";
 import authMiddleware from "../middlewares/AuthMiddleware";
+import updateTodo from "../controllers/updateTodo";
 
 const router = express.Router();
 
 router.post("/createTodo", authMiddleware, createTodo);
+router.post("/updateTodo", authMiddleware, updateTodo);
 
 export default router;
